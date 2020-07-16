ba2886f6474849e53c1103b1c73df9bafecf16d9
Zshail41 <zshailred41@gmail.com>
2020-07-16 18:38:57 -0300

    Updates

---
+++ precache-manifest.933d2c29b0b383740140f02605803c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c28d548d3dea99e095ee6caebce2f78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f71f109d67ce4dd7b6d1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cca55e8e0634ff2b5146",
+    "url": "/static/js/2.6bf0154d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6bf0154d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f71f109d67ce4dd7b6d1",
+    "url": "/static/js/main.75d177ed.chunk.js"
+  },
+  {
+    "revision": "60334080a3128d4a2926",
+    "url": "/static/js/runtime-main.d32eecf1.js"
+  }
+]);
